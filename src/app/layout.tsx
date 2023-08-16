700bd29372712fc7c88e69a40b9193853fcd1773
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-08-16 18:58:04 +0530

    added theme toggle

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,9 @@ import "@/styles/globals.css";
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 
+import { ThemeProvider } from "@/components/ThemeProvider";
+import { ModeToggle } from "@/components/ThemeToggle";
+
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
@@ -18,7 +21,12 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <main className="flex min-h-screen flex-col items-center justify-between">
-          {children}
+          <ThemeProvider attribute="class" defaultTheme="system" enableSystem>
+            <div className="absolute top-0 right-0 p-4">
+              <ModeToggle />
+            </div>
+            {children}
+          </ThemeProvider>
         </main>
       </body>
     </html>
