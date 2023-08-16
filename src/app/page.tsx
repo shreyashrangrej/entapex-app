5349e09441a97b5f241aa8779d3726dbe89d094e
Shreyash Rangrej <shreyashrangrej2000@gmail.com>
2023-08-16 11:45:53 +0530

    removed env

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,15 @@ import Link from "next/link";
 
 import { Button, buttonVariants } from "@/components/ui/button";
 import { cn } from "@/lib/utils";
+import { getCurrentUser } from "@/lib/session";
 
-export default function Home() {
+export default async function Home() {
+  const user = await getCurrentUser();
+
   return (
     <>
       <h1>EntApex App</h1>
+      <h2>Logged in as: {user?.email}</h2>
       <Link href="/login" className={cn(buttonVariants({ size: "lg" }))}>
         Get Started
       </Link>
